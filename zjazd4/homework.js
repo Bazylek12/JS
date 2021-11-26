af4d1b148464515efe8d5105a2d5db33de76a65d
Bazylek12 <a.kosmowski7@gmail.com>
2021-11-26 21:18:56 +0100

    homework

--- zjazd4/homework.js
+++ zjazd4/homework.js
@@ -1,192 +1,103 @@
 let data = require("./Data.json");
 
-let keyGetters = {
-    company: (x) => x.company,
-    type: (x) => x.type,
-    monthYear: (x) =>
-      x.date.getFullYear().toString() + "-" + (x.date.getMonth() + 1).toString(),
-    dayOfTheWeek: (x) => x.date.getDay(),
-    cost: (x) => x.cost,
-  };
+let dataGet = {
+  company: (x) => x.company,
+  type: (x) => x.type,
+  month: (x) => x.date.getFullYear().toString() + "-" + (x.date.getMonth() + 1).toString(),
+  day: (x) => x.date.getDay(),
+  cost: (x) => x.cost,
+};
 
-function Data (cost, type, company, date) {
-    this.cost = cost;
-    this.type = type;
-    this.company = company;
-    this.date = date;
+function Data(cost, type, company, date) {
+  this.cost = cost;
+  this.type = type;
+  this.company = company;
+  this.date = date;
 }
 
-data = data.map((x) => new Data(Number.parseFloat(x.cost),x.detailsOfPayent.Type,x.detailsOfPayent.company,parseToDate(x.detailsOfPayent.date)));
+function parseToDate(date) {
+  date = date.split("-");
+  return new Date(
+    date[2],
+    Number.parseInt(date[1]) - 1,
+    date[0]
+  );
+}
+data = data.map((x) => new Data(
+  Number.parseFloat(x.cost),
+  x.detailsOfPayent.Type,
+  x.detailsOfPayent.company,
+  parseToDate(x.detailsOfPayent.date)));
 
-function parseToDate(dateInString) {
-    dateInString = dateInString.split("-");
-    return new Date(
-      dateInString[2],
-      Number.parseInt(dateInString[1]) - 1,
-      dateInString[0]
-    );
-  }
-let newObj = {};
-for (i in data) {
-    let item = data[i];
-        if(newObj[item.key[0]] === undefined) {
-            newObj[item.key[0]] = 0;
-        }
-        newObj[item.key[0]] += item.value;
+
+function YearSpendings(year) {
+  return data.filter(x => x.date.getFullYear() === year).reduce((acc, value) => acc + value.cost, 0);
+};
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+};
+
+function companyEarn(company, cost) {
+  let unique = data.map(company).filter(onlyUnique).map((x) => {
+    return {
+      key: x,
+      value: 0
+    };
+  });
+  data.forEach((x) => (unique.find((y) => y.key === company(x)).value += cost(x)));
+  return unique;
 }
 
-let result = {};
-result.rows = [];
-for (i in newObj) {
-    result.rows.push({'key': i, 'value': newObj[i]});
+function spendingByType(type, cost) {
+  let unique = data.map(type).filter(onlyUnique).map((x) => {
+    return {
+      key: x,
+      value: 0
+    };
+  });
+  data.forEach((x) => (unique.find((y) => y.key === type(x)).value += cost(x)));
+  return unique;
+};
+
+function spendingByMonth(month, cost) {
+  let unique = data.map(month).filter(onlyUnique).map((x) => {
+    return {
+      key: x,
+      value: 0
+    };
+  });
+  data.forEach((x) => (unique.find((y) => y.key === month(x)).value += cost(x)));
+  return unique;
+};
+
+function spendingByDay(day, cost) {
+  let unique = data.map(day).filter(onlyUnique).map((x) => {
+    return {
+      key: x,
+      value: 0
+    };
+  });
+  data.forEach((x) => (unique.find((y) => y.key === day(x)).value += cost(x)));
+  return unique;
+};
+
+let ys = YearSpendings(2014);
+console.log(`Money spend in 2014: ${ys.toFixed(2)} \n`);
+let ce = companyEarn(dataGet.company, dataGet.cost);
+let sbt = spendingByType(dataGet.type, dataGet.cost);
+let sbm = spendingByMonth(dataGet.month, dataGet.cost);
+let sbd = spendingByDay(dataGet.day, dataGet.cost);
+
+for (let i = 0; i <= 2; i++) {
+  console.log(`Company: ${ce[i].key}, earnings: ${(ce[i].value).toFixed(2)} `);
 }
-console.log(result);
-// function getYearSpendings(year) {
-//     return data.filter((x) => x.date.getFullYear() === year).reduce((x, y) => x + y.cost, 0);
-// }
-// let ys = getYearSpendings(2014);
-// console.log(`Money spend in 2014: ${ys.toFixed(2)}`);
-
-// function CompanyEarnings(company, value) {
-//      return data.forEach((x) => (data.find((y) => y.company === company(x)).cost += value(x)));
-    
-// }
-// let ya = CompanyEarnings(this.company, this.cost);
-// console.log(ya);
-//console.log(data.forEach((x) => console.log(x.company, x.cost) ));
-
-
-
-// function onlyUnique(value, index, self) {
-//     return self.indexOf(value) === index;
-//   }
-  
-//   function agregate(data, keyFn, valueFn) {
-//     let uniques = data
-//       .map(keyFn)
-//       .filter(onlyUnique)
-//       .map((x) => {
-//         return { key: x, value: 0 };
-//       });
- 
-//     data.forEach(
-//       (x) => (uniques.find((y) => y.key === keyFn(x)).value += valueFn(x))
-//     );
-//     return uniques;
-//   }
-// let a = agregate(data, keyGetters.company, keyGetters.cost);
-// console.log(a)
-//   let unique = [...new Set(data.map(item => item.company))];
-//   console.log(unique)
-
-// let uniques = data.filter(onlyUnique).map((x) => {
-//     return { key: x, value: 0};
-// });
-// data.forEach((x) => (uniques.find((y) => y.key)));
-// console.log(uniques);
-
-// function agregate(data, keyFn, valueFn) {
-//   let uniques = data
-//     .map(keyFn)
-//     .filter(onlyUnique)
-//     .map((x) => {
-//       return { key: x, value: 0 };
-//     });
-
-//   data.forEach(
-//     (x) => (uniques.find((y) => y.key === keyFn(x)).value += valueFn(x))
-//   );
-//   return uniques;
-// }
-// agregate(data, x.company, x.cost);
-
-// function getYearSpendings(financialData, year) {
-//   return financialData
-//     .filter((x) => x.date.getFullYear() === year)
-//     .reduce((x, y) => x + y.cost, 0);
-// }
-
-
-
-// let data = getData("./Data.json");
-// data = data.map(
-//   (x) =>
-//     new Operation(
-//       Number.parseFloat(x.cost),
-//       x.detailsOfPayent.Type,
-//       x.detailsOfPayent.company,
-//       parseToDate(x.detailsOfPayent.date)
-//     )
-// );
-
-//data.forEach((p,i) => console.log(i, p.cost));
-
-// const personsJson = `{"1":{"firstName":"Jan","lastName":"Kowalski"},"2":{"firstName":"Justyna","lastName":"Kowalczyk"}}`;
-// const personsObject = JSON.parse(personsJson);
-// const personsMap = new Map(Object.entries(personsObject));
-// for (const key of personsMap.keys()) {
-//     console.log(key)
-// }
-
-
-
-   
-// let keyGetters = {
-//   company: (x) => x.company,
-//   type: (x) => x.type,
-//   monthYear: (x) =>
-//     x.date.getFullYear().toString() + "-" + (x.date.getMonth() + 1).toString(),
-//   dayOfTheWeek: (x) => x.date.getDay(),
-// };
-
-// let valueGetters = {
-//   cost: (x) => x.cost,
-// };
-
-// function Operation(cost, type, company, date) {
-//   this.cost = cost;
-//   this.type = type;
-//   this.company = company;
-//   this.date = date;
-// }
-
-
-
-// function getData(address) {
-//   return require(address);
-// }
-
-
-// function onlyUnique(value, index, self) {
-//   return self.indexOf(value) === index;
-// }
-// onlyUnique();
-
-
-// let ys = getYearSpendings(data, 2014); // pass value to funcion, not hordcode it inside it
-
-// let spendingsPerCompany = agregate(data, keyGetters.company, valueGetters.cost);
-
-// let spendingsPerTransaction = agregate(
-//   data,
-//   keyGetters.type,
-//   valueGetters.cost
-// );
-
-// let spendingsPerMonthYear = agregate(
-//   data,
-//   keyGetters.monthYear,
-//   valueGetters.cost
-// );
-
-// let spendingsPerDay = agregate(
-//   data,
-//   keyGetters.dayOfTheWeek,
-//   valueGetters.cost
-// );
-// let i = 0;
-// console.log(ys);
-// console.log(spendingsPerCompany);
-// console.log(spendingsPerTransaction);  
-// console.log(spendingsPerMonthYear);
-// console.log(spendingsPerDay)
+for (let i = 0; i <= 4; i++) {
+  console.log(`Type: ${(sbt[i].key)}, earnings: ${(sbt[i].value).toFixed(2)} `);
+}
+for (let i = 0; i <= 12; i++) {
+  console.log(`Month: ${(sbm[i].key)}, earnings: ${(sbm[i].value).toFixed(2)} `);
+}
+for (let i = 0; i <= 6; i++) {
+  console.log(`Day: ${(sbd[i].key)}, earnings: ${(sbd[i].value).toFixed(2)} `);
+}
