8cc8a32e8f008457573ee2610c8282e6edc68dde
Bazylek12 <a.kosmowski7@gmail.com>
2021-12-23 20:36:04 +0100

    zmiany

--- zjazd5/homework.js
+++ zjazd5/homework.js
@@ -2,7 +2,10 @@ let soldiers = [];
 for (let i = 1; i <= 7; i++) {
     soldiers.push(i);
 }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 7671205 (zmiany)
 let alive;
 let dead;
 
@@ -16,4 +19,29 @@ while (soldiers.length > 1) {
     console.log(`${alive} kill ${dead}`);
 }
 
+<<<<<<< HEAD
 console.log(`${soldiers[0]} remains alive `);
+=======
+console.log(`${soldiers[0]} remains alive `);
+
+// // populate the array for 100 people
+// var team = new Array();
+// for (var i = 1; i <= 100; i++) {
+//  team[i - 1] = i;
+// }
+
+// var current;
+// var next;
+
+// var j = 0.
+// while (team.length > 1) { // loop until only 1 exists
+//  current = team[j];
+//  team.push(team[j]); // move current to the end
+//  team.shift(); // remove from existing
+//  next = team[j];
+//  team.shift(); // delete next
+//  document.writeln("Number " + current + " kills number " + next + "<br />");
+// }
+// document.writeln("The last one is number " + team[0]);
+// //alert(team[0]);
+>>>>>>> 7671205 (zmiany)
